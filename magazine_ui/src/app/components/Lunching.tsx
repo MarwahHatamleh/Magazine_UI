18658b29e507c5d741dbb82e8759386d47d3f5c8
MarwahHatamleh <marwahatamleh98@gmail.com>
2025-11-04 20:59:29 +0300

    lunching design hrml/css

---
+++ magazine_ui/src/app/components/Lunching.tsx
@@ -0,0 +1,33 @@
+const Lunching = () => {
+  return (
+    <>
+      <div className="slider">
+        <div className="slides">
+          <div className="slide s1">
+            <div className="warpperDiv">
+              <span className="header">GET READY</span>
+              <div className="str1">SOMETHING</div>
+              <div className="str2">UNIQUE</div>
+              <div className="str3">IS COMING</div>
+            </div>
+          </div>
+          <div className="slide">
+            <div className="warpperDiv">
+              <span className="header">we are</span>
+              <div className="slide2_str1">Lunching</div>
+              <div className="slide2_str2">SOON</div>
+            </div>
+          </div>
+          <div className="slide">
+            <div className="warpperDiv">
+              <div className="slide3_str1">OUR STORY</div>
+              <div className="slide3_str2">BEGINS</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Lunching;
---
+++ magazine_ui/src/app/lunching/page.tsx
@@ -0,0 +1,12 @@
+import Lunching from "../components/Lunching";
+import "./style.css";
+
+const magazine = () =>{
+return (
+    <>
+    <Lunching/>
+    </>
+)
+}
+
+export default magazine;
